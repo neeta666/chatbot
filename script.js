519b489d591643a32cffd9b3d4f2084a6a1ef598
neeta666 <128306301+neeta666@users.noreply.github.com>
2023-06-27 04:03:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+//const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
